a6bbaf854a46c17e2c2fa876ee767e730cab3ddf
agwadan <agwa.dan@gmail.com>
2022-02-09 14:34:34 +0300

    Pass props from app component to Header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,21 @@
 <template>
   <header>
-    <h1>Task Tracker</h1>
+    <h1>{{ title }}</h1>
   </header>
 </template>
 
 <script>
 export default {
   name: "Header",
+  props: {
+    title: String,
+    default: "Hello World",
+  },
 };
 </script>
 
 <style scoped>
+/* Scoped means this styling applies only to this component */
 header {
   display: flex;
   justify-content: space-between;
