aa795e6d6e897db7d2386b90f421f4b4dc6e0342
agwadan <agwa.dan@gmail.com>
2022-04-04 09:15:52 +0300

    Update task reminder in db

--- src/App.vue
+++ src/App.vue
@@ -69,9 +69,20 @@ export default {
       this.tasks = this.tasks.filter((task) => task.id !== id);
     },
 
-    toggleReminder(id) {
+    async toggleReminder(id) {
+      const taskToToggle = await this.fetchTask(id);
+      const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+
+      const res = await fetch(`api/tasks/${id}`, {
+        method: "PUT",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify(updTask),
+      });
+
+      const data = await res.json();
+
       this.tasks = this.tasks.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task
+        task.id === id ? { ...task, reminder: data.reminder } : task
       );
     },
 
